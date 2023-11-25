853c36670ceab90052d62c395d5a06e3bb86643e
Shivani@7721 <shivanibansod11@gmail.com>
2023-11-25 14:57:22 +0530

    first-commit

---
+++ src/main/java/com/example/demospringappservice/DemoSpringAppServiceApplication.java
@@ -0,0 +1,21 @@
+package com.example.demospringappservice;
+
+import gitorg.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoSpringAppServiceApplication {
+
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats! Your App is deployed!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringAppServiceApplication.class, args);
+	}
+
+}
